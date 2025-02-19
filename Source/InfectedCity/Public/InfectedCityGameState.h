72d7414cf25736a23dd33c66b97fd00da8ff7837
Cat-Yubi <kimtaewan144@gmail.com>
2025-02-19 20:26:18 +0900

    Update Scope

---
+++ Source/InfectedCity/Public/InfectedCityGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "InfectedCityGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFECTEDCITY_API AInfectedCityGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
